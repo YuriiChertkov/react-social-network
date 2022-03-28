fa1369661220e841a565731268c25f88242737e4
YuriiChertkov <yuriichertkov@gmail.com>
2022-03-28 11:11:02 +0300

    profile selector

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
+import Preloader from "../../common/preloader";
 
 const ProfileInfo = (props) => {
+  if (!props.profile){
+    return <Preloader/>
+  }
   return (
     <div>
       <div className={s.img}>
         <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"  alt="Background"/>
       </div>
-      <div className={s.profileinfo}>ava+descripton</div>
+      <div className={s.profileinfo}>
+        <div><img src={props.profile.photos.large}/></div>
+        Contacts: <img src={props.profile.photos.small}/>
+        </div>
     </div>
   );
 };
