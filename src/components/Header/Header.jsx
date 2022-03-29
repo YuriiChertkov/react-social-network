9742ef395139d8f93836d5fd333bbba957ff5476
YuriiChertkov <yuriichertkov@gmail.com>
2022-03-30 00:20:12 +0300

    login navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import s from'./Header.module.css';
+import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <header className={s.header}>
       <img src="http://www.freelogovector.com/wp-content/uploads/2017/06/01%20-%20PNG%20My%20Company%20copy.jpg" />
+      <div className={s.login}>
+        <NavLink to ={'/login'}>
+            Login
+        </NavLink>
+         </div>
+      
     </header>
   );
 };
