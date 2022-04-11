90b80324454c2944a282f416dd16447cb2feb423
YuriiChertkov <yuriichertkov@gmail.com>
2022-04-11 09:51:30 +0300

    temp 2

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,22 +1,16 @@
-import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
 import Profile from "./Profile";
-import { setProfileUser } from "../../redux/profile_reducer";
+import { getProfileUser } from "../../redux/profile_reducer";
 import { withRouter } from "react-router-dom";
-
 class ProfileContainer extends React.Component {
   componentDidMount() {
+  
     let userId = this.props.match.params.userId;
     if (!userId) {
       userId = 2;
     }
-
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
-      .then((response) => {
-        this.props.setProfileUser(response.data);
-      });
+    this.props.getProfileUser(userId);
   }
 
   render() {
@@ -36,4 +30,4 @@ let mapStateToProps = (state) => {
 
 let withUrlDataCC = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, { setProfileUser })(withUrlDataCC);
+export default connect(mapStateToProps, { getProfileUser })(withUrlDataCC);
--- src/redux/profile_reducer.js
+++ src/redux/profile_reducer.js
@@ -1,3 +1,5 @@
+import { usersAPI } from "../api/api";
+
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 const SET_PROFILE_USER = "SET_PROFILE_USER";
@@ -29,8 +31,7 @@ const profileReducer = (state = initialState, action) => {
       return { ...state, newPostText: action.newText };
     }
     case SET_PROFILE_USER: {
-      return{ ...state, profile: action.profile,
-      }
+      return { ...state, profile: action.profile };
     }
     default:
       return state;
@@ -50,10 +51,17 @@ export const updateNewPostTextActionCreator = (text) => {
   };
 };
 
-export const setProfileUser = (profile)=>{
-  return{
-    type: SET_PROFILE_USER, profile
-  }
-}
+export const setProfileUser = (profile) => {
+  return {
+    type: SET_PROFILE_USER,
+    profile,
+  };
+};
+
+export const getProfileUser = (userId) => (dispatch) => {
+  usersAPI.getProfile(userId).then((response) => {
+    dispatch(setProfileUser(response.data));
+  });
+};
 
 export default profileReducer;
